f11e9cadfd667981de32ac41414b4e8a5cf0ebc3
Amirul9135 <amirul.asraf9135@gmail.com>
2023-01-09 23:10:25 +0800

    address

--- Controller/addressApi.js
+++ Controller/addressApi.js
@@ -65,4 +65,65 @@ router.get("/area", function (req, res) {
     })
 })
 
+
+router.get("/geo/area", function (req, res) {
+    if (!req.query.id) {
+        return res.status(400).send()
+    }
+    Address.getAreaGeo(req.query.id).then(function (result) {
+        return res.status(200).send(result)
+    }).catch(function (err) {
+        return res.status(500).send({ error: err })
+    })
+})
+
+
+router.post("/geo/area", [
+    Validator.checkNumber('areaId', { min: 0 }),
+    Validator.validate()
+], function (req, res) {
+    console.log(req.body)
+    if (!req.body.coord || !req.body.areaPolygon)
+        return res.status(400).send({ validationError: { coord: 'must include coordinate and polygon' } })
+    if (!validateCoord(req.body.coord))
+        return res.status(400).send({ validationError: { point: 'invalid coordinate' } })
+    for (var i = 0; i < req.body.areaPolygon[0].length; i++) {
+        if (!validateCoord(req.body.areaPolygon[0][i]))
+            return res.status(400).send({ validationError: { point: 'invalid coordinate' } })
+    }
+    //sampai sini dah valid da coord2 ni 
+    //lastpoint
+    var lp = req.body.areaPolygon[0][req.body.areaPolygon[0].length - 1]
+    //first point
+    var fp = req.body.areaPolygon[0][0]
+    if ((lp[Object.keys(lp)[0]] != fp[Object.keys(fp)[0]]) || (lp[Object.keys(lp)[1]] != fp[Object.keys(fp)[1]])) {
+        //if either lat long x y x equal diff point push ah sbb polygon kene start end at same point sql standard otherwise null
+        //auto tutup function haha
+        req.body.areaPolygon[0].push(fp)
+    }
+    console.log(JSON.stringify(req.body))
+    Address.updateGeo(req.body).then(function (result) {
+        return res.status(200).send(result)
+    }).catch(function (err) {
+        return res.status(500).send({ error: err })
+    })
+})
+
+
+function validateCoord(coordObj) { //long lat x y as key 
+    if (Object.keys(coordObj).length != 2) {
+        return false
+    }
+    if (typeof (coordObj[Object.keys(coordObj)[0]]) != 'number' || typeof (coordObj[Object.keys(coordObj)[1]]) != 'number')
+        return false
+    if (coordObj[Object.keys(coordObj)[0]] > 180 || coordObj[Object.keys(coordObj)[0]] < -180) {
+        //first key long/x max -180~180
+        return false
+    }
+    if (coordObj[Object.keys(coordObj)[1]] > 90 || coordObj[Object.keys(coordObj)[1]] < -90) {
+        return false
+    }
+    return true
+}
+
 module.exports = router;
