94093f763aec3da79bb8f49f214665cb2531b1a6
Amirul9135 <amirul.asraf9135@gmail.com>
2023-01-13 16:18:59 +0800

    fidn student query fix

--- Model/entity/Student.js
+++ Model/entity/Student.js
@@ -36,20 +36,22 @@ module.exports = class Student {
         });
     }
 
-    static getStudent(schoolId, classId) {
+    static getStudent(schoolId, classId, year = null) {
         return new Promise(function (resolve, reject) {
             //var strSql ="SELECT a.*, b.* FROM (SELECT s.studentId, a.name FROM account a INNER JOIN student s ON s.studentId = a.accountId) a JOIN class_student cs ON a.studentId = cs.studentId JOIN (SELECT c.className,c.classId,s.fullName,s.schoolId FROM class c INNER JOIN school s ON s.schoolId = c.schoolId) b ON cs.classId = b.classId ";
             var strSql = "SELECT a.*,b.*,MAX(cs.startDate) FROM (SELECT s.studentId, a.name,a.disabled FROM account a INNER JOIN student s ON s. studentId=a.accountId) a JOIN class_student cs ON a.studentId=cs.studentId JOIN (SELECT c.className,c.classId,s.fullName,s.schoolId FROM class c INNER JOIN school s ON s.schoolId = c.schoolId) b ON cs.classId=b.classId"
 
             strSql += " WHERE b.schoolId = " + db.escape(schoolId)
             if (classId) {
-
                 strSql += " AND b.classId = " + db.escape(classId)
             }
+            if (!year) {
+                strSql += " AND cs.startDate <= CURRENT_DATE AND cs.endDate > CURRENT_DATE "
+            }
             strSql += " GROUP BY a.studentId"
+            console.log(strSql)
             db.query(strSql, function (err, result) {
                 if (err) {
-                    console.log("error:" + err.message);
                     reject(err.message);
                 }
                 else {
